b05a53c5b08a38eb7af1e1fbba966e763142497d
Melat Worku <76426996+LadyMelz@users.noreply.github.com>
2022-05-22 16:19:54 +0300

    Add files via upload

---
+++ IP2/account/footer2.php
@@ -0,0 +1,16 @@
+<div class="wrap4">
+  <div class="container">
+    <div class="footer">
+      <a href="https://www.facebook.com/people/Lomi-Da/100075870416279/"><i class="bi bi-facebook"></i></a>   
+      <a href="https://twitter.com/DatingLomi"><i class="bi bi-twitter"></i></a>
+      <a href=""><i class="bi bi-instagram"></i></a>
+      <a href="mailto:lomidating@gmail.com"><i class="bi bi-google"></i></a>
+       <p>Copyright� Lomi Dating Site 2021, All rights reserved.</p><br />
+    </div>
+  <div class="clearing"></div>
+  </div>
+  </div>
+<div class="shadows2">
+</div>        
+    </body>
+</html>
---
+++ IP2/account/header2.php
@@ -0,0 +1,33 @@
+<body>
+
+
+    <div class="wrap1">
+        <div class="container">
+          <div class="header">   
+            <div class="accountmenu">
+              
+              <a id="Logo" href="../index.html"><img src="../images/Logo.png"class="logo"></a>
+              <div class="textbox">
+                <input type="text" placeholder="Search">
+                <i class="bi bi-search"></i>
+              </div>
+              <div class="accountimg" >
+                <div class="submenu">    
+                  <ul>
+                     <li><a href="../account.html"><i class="bi bi-house-fill"></i>Home</a></li>
+                     <li><a href="Friends.html"><i class="bi bi-people-fill"></i>Friends</a></li>s
+                     <li><a href="setting.html"><i class="bi bi-gear-fill"></i>Setting</a></li>
+                      <li><a onclick="logout2()"><i class="bi bi-box-arrow-right"></i>Logout</a></li>
+                      </ul>
+                    </div>
+              <button ><img src="../images/maleavatar.png" ></button>
+              </div>
+            </div>
+          </div>
+          <div class="clearing"></div>
+        </div>
+        </div>
+
+        //<?php
+        //$username = $_POST["username"];
+        //?>
